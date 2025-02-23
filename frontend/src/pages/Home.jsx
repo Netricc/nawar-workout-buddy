00dc68ab5efad93431f57962c83dae85f26d3435
Netric Master <147075331+Netricc@users.noreply.github.com>
2025-02-23 17:39:56 +0100

    Update Home.jsx

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -6,7 +6,7 @@ const Home = () => {
   const { workouts, dispatch } = useWorkoutsContext();
   useEffect(() => {
     const fetchWorkouts = async () => {
-      const response = await fetch("/api/workouts/");
+      const response = await fetch("https://nawar-workout-buddy.vercel.app/api/workouts/");
       const json = await response.json();
 
       if (response.ok) {
