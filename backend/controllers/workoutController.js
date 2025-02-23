19b8fadbb64a398f0a45d93df9aa3b648e2d4502
Nawar <nawabsn911@gmail.com>
2025-02-23 17:31:19 +0100

    app V.1

---
+++ backend/controllers/workoutController.js
@@ -0,0 +1,105 @@
+const Workout = require("../models/workoutModel");
+const mongoose = require("mongoose");
+// Get all Workouts
+const getWorkouts = async (req, res) => {
+  const workouts = await Workout.find({}).sort({ createdAt: -1 });
+
+  res.status(200).json(workouts);
+};
+
+// Get a signle Workout
+const getWorkout = async (req, res) => {
+  const { id } = req.params;
+
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return res.status(404).json({ error: "No such workout" });
+  }
+
+  const workout = await Workout.findById(id);
+
+  if (!workout) {
+    return res.status(404).json({ error: "No such workout" });
+  }
+  res.status(200).json(workout);
+};
+
+// create new workout
+const createWorkout = async (req, res) => {
+  const { title, load, reps } = req.body;
+
+  let emptyFields = [];
+
+  if (!title) {
+    emptyFields.push("title");
+  }
+  if (!load) {
+    emptyFields.push("load");
+  }
+  if (!reps) {
+    emptyFields.push("reps");
+  }
+  if (emptyFields.length > 0) {
+    return res
+      .status(400)
+      .json({ error: "Please fill in all the fields", emptyFields });
+  }
+
+  try {
+    const workout = await Workout.create({
+      title,
+      load,
+      reps,
+    });
+    res.status(200).json(workout);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+};
+
+// delete a workout
+const deleteWorkout = async (req, res) => {
+  const { id } = req.params;
+
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return res.status(404).json({ error: "No such workout" });
+  }
+
+  const workout = await Workout.findOneAndDelete({ _id: id });
+
+  if (!workout) {
+    return res.status(404).json({ error: "No such workout" });
+  }
+
+  res.status(200).json(workout);
+};
+
+// updat a workout
+
+const updateWorkout = async (req, res) => {
+  const { id } = req.params;
+
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return res.status(404).json({ error: "No such workout" });
+  }
+
+  const workout = await Workout.findByIdAndUpdate(
+    { _id: id },
+    {
+      ...req.body,
+    }
+  );
+
+  if (!workout) {
+    return res.status(404).json({ error: "No such workout" });
+  }
+
+  res.status(200).json(workout);
+};
+
+module.exports = {
+  getWorkout,
+  getWorkouts,
+  createWorkout,
+  deleteWorkout,
+  updateWorkout,
+};
---
+++ backend/server.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const cors = require("cors");
+const workoutRoutes = require("./routes/workouts");
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+const app = express();
+app.use(express.json());
+app.use(
+  cors({
+    origin: [
+      "http://localhost:3000/",
+      "https://nawra-workout-buddy.vercel.app",
+    ],
+    methods: ["POST", "GET", "DELETE"],
+    credentials: true,
+  })
+);
+
+app.use((req, res, next) => {
+  console.log(req.path, req.method);
+  next();
+});
+
+app.use("/api/workouts", workoutRoutes);
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log("Connected to the Database");
+    app.listen(process.env.PORT, () => {
+      console.log(`Listening on port ${process.env.PORT}`);
+      console.log(`http://localhost:${process.env.PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.log("Connect faild to the Database: ", error);
+  });
